bb62cc934018ec6ed50c4e7bf41e07586396ed39
adrianleewt <adrianlee1111@gmail.com>
2021-04-20 11:20:15 +0800

    Initial cleaning

--- py/cleaning.py
+++ py/cleaning.py
@@ -1,30 +1,23 @@
 import pandas as pd
 pd.set_option('display.max_rows', None)
 data = pd.read_csv("../data/states_all_extended.csv")
+scores = pd.read_csv("../data/naep_states_summary.csv")
 
-#print(data)
+selected_data = data[['PRIMARY_KEY','STATE','YEAR','ENROLL','TOTAL_REVENUE','FEDERAL_REVENUE','STATE_REVENUE','LOCAL_REVENUE','TOTAL_EXPENDITURE','INSTRUCTION_EXPENDITURE','SUPPORT_SERVICES_EXPENDITURE','OTHER_EXPENDITURE','CAPITAL_OUTLAY_EXPENDITURE']]
+scores = scores.drop(columns=['STATE','YEAR'])
 
-#print(data.describe())
+result = selected_data.merge(scores, left_on='PRIMARY_KEY', right_on='PRIMARY_KEY')
+# result['AVG_SCORE'] = result['AVG_MATH_4_SCORE'] + result['AVG_MATH_8_SCORE'] + result['AVG_READING_4_SCORE'] + result['AVG_READING_8_SCORE']
 
-#print(data)
+result.to_csv('../data/filtered.csv', index=False)
 
-#print(data[data['YEAR']>2000].isna().sum())
-columns = list(data.columns.values.tolist())
+# #print(len(pd.unique(data['STATE'])))
+# for i in range(1992, 2020, 1):
+#     filtered_data = data[data['YEAR'] == i]
+#     # print(filtered_data)
+#     # print(filtered_data.isna().sum())
 
-na_frame = pd.DataFrame(columns)
-
-#print(len(pd.unique(data['STATE'])))
-for i in range(1992, 2020, 1):
-    filtered_data = data[data['YEAR'] == i]
-    # print(filtered_data)
-    # print(filtered_data.isna().sum())
-
-    na_frame[str(i) + '_NAs'] = list(filtered_data.isna().sum())
+#     frame[str(i) + '_NAs'] = list(filtered_data.isna().sum())
 
 
-na_frame.to_csv('../data/output_full.csv')
 # 1715 rows
-
-
-#for i in range(266):
-#   data[:i]
