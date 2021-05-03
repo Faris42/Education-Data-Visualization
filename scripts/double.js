ecb1d697b689ca407b694aa97cdd4ffba556ca38
Faris Aziz <fsa22@cornell.edu>
2021-05-02 22:35:41 -0400

    Initial push

--- scripts/double.js
+++ scripts/double.js
@@ -1,70 +1,29 @@
-async function renderDouble(sd, stateNames, yrs, activeYear) {
-	// //console.log('double render', sd, stateNames, yrs, activeYear);
-	// const data = await d3.csv('/data/final.csv');
-	// data.forEach((d) => {
-	//   d.YEAR = parseInt(d.YEAR);
-	//   d.AVG_SCORE = parseInt(d.AVG_SCORE);
-	//   d.GDP = parseInt(d.GDP);
-	//   d.GDP_PER_CAPITA = parseFloat(d.GDP_PER_CAPITA);
-	//   d.INSTRUCTION_EXPENDITURE = parseInt(d.INSTRUCTION_EXPENDITURE);
-	// });
-	// const yearData = data.filter(function (point) {
-	//   return point.YEAR === activeYear;
-	// });
-	// let state1 = yearData.filter(function (point) {
-	//   return point.STATE === stateNames[0].toUpperCase();
-	// });
-	// let state2 = yearData.filter(function (point) {
-	//   return point.STATE === stateNames[1].toUpperCase();
-	// });
+let values;
+let div;
+let svg;
+let width;
+let height;
+let style;
 
-	// state1 = state1[0];
-	// state2 = state2[0];
+let margin;
+let chartWidth;
+let chartHeight;
+let chartArea;
+let percentScale;
 
-	// console.log(data);
-	// console.log(yearData);
 
-	// console.log(state1);
-	// console.log(state2);
+function renderDouble(sd, stateNames, yrs, activeYear) {
 
-	// console.log(state1.AVG_SCORE);
-
-	// console.log('gdp sd function', stateNames[0], activeYear);
-	// // console.log(sd.getGDP(stateNames[0], activeYear));
-	// // console.log(state1.GDP);
-	// // console.log(state1.GDP_PER_CAPITA);
-	// // console.log(state1.INSTRUCTION_EXPENDITURE);
-
+  let first = true;
+  if(first == true) {
+    
+    setupDouble();
+    first = false;
+  }
+  
 	const state1 = sd.getRecord(stateNames[0], activeYear);
-	const state2 = sd.getRecord(stateNames[1], activeYear);
-
-	console.log(state1.GDP);
-	console.log(state2.GDP);
-	//console.log(sd.getRecord(stateNames[0], activeYear));
-
-	const values = ["GDP", "GDP_PER_CAPITA", "INSTRUCTION_EXPENDITURE"];
-
-	const div = d3.select("div#screen_double");
-	// div.style("display", "block");
-
-	const svg = d3.select("svg#doubleSvg");
-	const width = svg.attr("width");
-	const height = svg.attr("height");
-	const style = svg.attr("style");
-	const margin = { top: 10, right: 10, bottom: 10, left: 10 };
-	const chartWidth = width - margin.left - margin.right;
-	const chartHeight = height - margin.top - margin.bottom;
-
-	let annotations = svg.append("g").attr("id", "annotations");
-	let chartArea = svg
-		.append("g")
-		.attr("id", "points")
-		.attr("transform", `translate(${margin.left},${margin.top})`);
-
-	const percentScale = d3.scaleLinear().domain([0, 1]).range([0, chartWidth]);
-
-	// const percentScale = d3.scaleLinear().domain([0,1]).range([0, chartWidth]);
-
+  const state2 = sd.getRecord(stateNames[1], activeYear);
+  
 	let yCounter = 30;
 	values.forEach(function (v) {
 		let value1 = state1[v];
@@ -72,11 +31,6 @@ async function renderDouble(sd, stateNames, yrs, activeYear) {
 
 		let percent1 = value1 / (value1 + value2);
 		let percent2 = value2 / (value1 + value2);
-
-		// console.log(value1);
-		// console.log(percent1);
-		// console.log(value2);
-		// console.log(percent2);
 
 		let text = v.replaceAll("_", " ");
 		text = text.toLowerCase();
@@ -92,31 +46,96 @@ async function renderDouble(sd, stateNames, yrs, activeYear) {
 			.style("font-weight", "bold")
 			.text(text);
 
-		chartArea
-			.append("rect")
-			.attr("x", 0)
-			.attr("y", yCounter)
-			.attr("width", percentScale(percent1))
-			.attr("height", 50)
-			.attr("fill", "red");
+		// chartArea
+		// 	.append("rect")
+		// 	.attr("x", 0)
+		// 	.attr("y", yCounter)
+		// 	.attr("width", percentScale(percent1))
+		// 	.attr("height", 50)
+		// 	.attr("fill", "#16b6eb");
 
-		chartArea
-			.append("rect")
-			.attr("x", percentScale(percent1))
-			.attr("y", yCounter)
-			.attr("width", percentScale(percent2))
-			.attr("height", 50)
-			.attr("fill", "blue");
+		// chartArea
+		// 	.append("rect")
+		// 	.attr("x", percentScale(percent1))
+		// 	.attr("y", yCounter)
+		// 	.attr("width", percentScale(percent2))
+		// 	.attr("height", 50)
+		// 	.attr("fill", "#2979a3");
 
-		// let path = d3.path();
 
-		// path.moveTo(percentScale(0.5), yCounter-10);
-		// path.lineTo(percentScale(0.5), yCounter+59);
+		// chartArea
+		// 	.append("line")
+		// 	.attr("x1", percentScale(0.5))
+		// 	.attr("x2", percentScale(0.5))
+		// 	.attr("y1", yCounter - 10)
+		// 	.attr("y2", yCounter + 59)
+		// 	.style("stroke-dasharray", "5,5") //dashed array for line
+		// 	.style("stroke", "black");
 
-		// chartArea.append("path")
-		//           .attr("class", "line")
-		//           .attr("d", path)
-		//           .style("stroke-dasharray", ("3, 3"))
+		// chartArea
+		// 	.append("text")
+		// 	.attr("x", percentScale(percent1) - 5)
+		// 	.attr("y", yCounter + 60)
+		// 	.attr("text-anchor", "end")
+		// 	.attr("dominant-baseline", "hanging")
+		// 	.style("font", "14px Arial")
+		// 	.text(value1);
+
+		// chartArea
+		// 	.append("text")
+		// 	.attr("x", percentScale(percent1) + 5)
+		// 	.attr("y", yCounter + 60)
+		// 	.attr("text-anchor", "start")
+		// 	.attr("dominant-baseline", "hanging")
+		// 	.style("font", "14px Arial")
+    // 	.text(value2);
+    const leftName = "rect.left."+v;
+    chartArea.selectAll(leftName)
+      .join( enter => enter.append("rect")
+                            .attr("x", 0)
+                            .attr("y", yCounter)
+                            .attr("width", percentScale(percent1))
+                            .attr("height", 50)
+                            .attr("fill", "#16b6eb")
+                            .attr("opacity", 0)
+                            .call( enter => enter.transition().attr('opacity', 1)),
+              update => update.call( update => update.transition()
+                                                    .attr("x", 0)
+                                                    .attr("y", yCounter)
+                                                    .attr("width", percentScale(percent1))
+                                                    .attr("height", 50)
+                                                    .attr("fill", "#16b6eb")),
+              exit => exit.call( exit => exit.transition().attr('opacity', 0).remove));
+    
+    const rightName = "rect.right."+v;
+    chartArea.selectAll(rightName)
+      .join( enter => enter.append("rect")
+                            .attr("x", percentScale(percent1))
+                            .attr("y", yCounter)
+                            .attr("width", percentScale(percent2))
+                            .attr("height", 50)
+                            .attr("fill", "#2979a3")
+                            .attr("opacity", 0)
+                            .call( enter => enter.transition().attr('opacity', 1)),
+              update => update.call( update => update.transition()
+                                                      .attr("x", percentScale(percent1))
+                                                      .attr("y", yCounter)
+                                                      .attr("width", percentScale(percent2))
+                                                      .attr("height", 50)
+                                                      .attr("fill", "#2979a3")),
+              exit => exit.call( exit => exit.transition().attr('opacity', 0).remove));
+    
+
+    console.log(leftName);
+    console.log(rightName);
+		// chartArea
+		// 	.append("rect")
+		// 	.attr("x", percentScale(percent1))
+		// 	.attr("y", yCounter)
+		// 	.attr("width", percentScale(percent2))
+		// 	.attr("height", 50)
+		// 	.attr("fill", "#2979a3");
+
 
 		chartArea
 			.append("line")
@@ -148,6 +167,32 @@ async function renderDouble(sd, stateNames, yrs, activeYear) {
 		yCounter += 150;
 	});
 	return;
+}
+
+function setupDouble() {
+
+	values = ["GDP", "GDP_PER_CAPITA", "INSTRUCTION_EXPENDITURE"];
+
+	div = d3.select("div#screen_double");
+  svg = d3.select("svg#doubleSvg");
+
+  svg.selectAll('g').remove();
+  
+	width = svg.attr("width");
+	height = svg.attr("height");
+	style = svg.attr("style");
+	margin = { top: 10, right: 10, bottom: 10, left: 10 };
+	chartWidth = width - margin.left - margin.right;
+	chartHeight = height - margin.top - margin.bottom;
+
+  //let annotations = svg.append("g").attr("id", "annotations");
+	chartArea = svg
+		.append("g")
+		.attr("id", "points")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+    
+  percentScale = d3.scaleLinear().domain([0, 1]).range([0, chartWidth]);
+  
 }
 
 function capitalize_Words(str) {
